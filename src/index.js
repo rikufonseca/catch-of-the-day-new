9dd105d9e85adaecde95db1d76a0bb1801dc6d52
erika fonseca <fonsecarika@gmail.com>
2023-12-23 12:49:16 +0200

    first push

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Router from '../components/Router'
+import { render } from 'react-dom'
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+render(<Router/>, document.querySelector("#root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

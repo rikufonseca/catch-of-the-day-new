0aae1cfd136371fb068a1ca41e3f9932eb228c52
erika fonseca <fonsecarika@gmail.com>
2023-12-29 20:42:07 +0200

    new component

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+	render() {
+		return (
+			<form className="fish-edit">
+        <input name='name' type="text" placeholder="Name"/>
+        <input name='price' type="text" placeholder="Price"/>
+        <input name='status' type="text" placeholder="Status"/>
+        <input name='desc' type="text" placeholder="Desc"/>
+        <button type="submit"> + Add Fish </button>
+      </form>
+		);
+	}
+}
+
+export default AddFishForm;
